073cf8483ccb4efdca6254c950653a943912677e
Hassan Hussien <hakpb7@gmail.com>
2026-06-16 04:44:00 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,14 +6,11 @@ const app = express();
 const tourRoutes = require('./routes/tourRoutes');
 const usersRouter = require('./routes/userRoutes');
 
-// use morgan & jsonParser middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use(express.static(`${__dirname}/../public`));
 
-// routes
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', usersRouter);
 
